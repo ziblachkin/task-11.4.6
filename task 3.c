d45d172157b2109167ac09edce2d06cbb752a4f1
Daniil Breslavskyi <danosnyt2115@gmail.com>
2025-07-04 15:52:31 +0200

    in uni , can't load

--- task 3.c
+++ task 3.c
@@ -25,13 +25,13 @@ char lowl_save(LOWL *list, char *filename){
 	OWN *tmp = list->first_element;
 	
 	
-	FILE *file = fopen(filename,"wb");
+	FILE *file = fopen("list.check","wb");
 
 	if(file == 0){
 		return LOWL_FILE_PROBLEM;
 	}
 	
-	while(tmp != list->current_element){
+	while(tmp != 0){//list->current_element
 		count ++;
 		tmp = tmp->next;
 	}
@@ -41,14 +41,14 @@ char lowl_save(LOWL *list, char *filename){
 		return LOWL_FILE_PROBLEM;
 	}
 	
-	if(fwrite(count, sizeof(int), 1, file) != 1){//check posledovatelnost: L O W L, int/count(1), then elements
+	if(fwrite(&count, sizeof(int), 1, file) != 1){//check posledovatelnost: L O W L, int/count(1), then elements
 		fclose(file);
 		return LOWL_FILE_PROBLEM;
 	}
 	
 	tmp = list->first_element;
 	while(tmp != 0){
-		if(fwrite(&tmp->element,sizeof(float),1,file) != 1){//incompatible type of arg?? 
+		if(fwrite(&tmp->element,sizeof(float),1,file) != 1){//incompatible type of arg?? ok now
 			fclose(file);
 			return LOWL_FILE_PROBLEM;
 		}
@@ -64,41 +64,53 @@ char lowl_save(LOWL *list, char *filename){
 LOWL* lowl_load(char *filename){
 	
 	int i = 0;
-	//OWN *previous_element = 0; use at the end of the days) 
+	int count = 0;
+	char check_the_lowl_2[5] = {0};
+	OWN *previous_element = 0; 
 	LOWL *list = malloc(sizeof(LOWL));
 	OWN *tmp = list->first_element;
-	char check_the_lowl_2[4];
-	int count = 0;
 	
-	FILE *file = fopen(filename, "rb");
+	
+	//list->first_element = malloc(sizeof(OWN));
+	//list->current_element = malloc(sizeof(OWN));//
+	
+	FILE *file = fopen("list.check", "rb");
 	
 	if(file == 0){
+		printf("here 1");
 		fclose(file);
 		return LOWL_FILE_PROBLEM;
 	}
 	
 	if(fread(&check_the_lowl_2, sizeof(char), 4, file) != 4){//4 chars and writing it to ctl 2
+		printf("here 2");
 		fclose(file);
 		return LOWL_FILE_PROBLEM;
 	}
 	
 	if(strcmp(check_the_lowl_2, "LOWL") != 0){//check same name
+		printf("here 3");
 		fclose(file);
 		return LOWL_FILE_PROBLEM;
 	}
 	
 	if(list == 0){
+		printf("here 3");
 		fclose(file);
-		return LOWL_FILE_PROBLWM;
+		return LOWL_FILE_PROBLEM;
 	}
 	
-	list->first_element = malloc(sizeof(OWN));//
 	
 	if(list->first_element == 0){
+		printf("here 4");
 		free(list);
 		fclose(file);
 		return LOWL_FILE_PROBLEM;
 	}
+	
+	printf("col 111 is going good\n");
+	
+	list->first_element = malloc(sizeof(OWN));
 	
 	while(fread(&tmp->element,sizeof(float),1,file) == 1){
 		tmp->next = malloc(sizeof(OWN));
@@ -111,17 +123,75 @@ LOWL* lowl_load(char *filename){
 			free(list);
 			fclose(file);
 		}
-
 	}
 	
+	printf("col 128 going good\n");
 	
 	for(i = 0; i < count; i++){
 		list->current_element = list->current_element->next;
 	}
 	
+	printf("lowl_load b4 free and close went good\n");
+	
 	free(tmp);
 	fclose(file);
-	
-	return list;
+		
+	//return list;
 }
 
+int main(){
+	
+	int count_2 = 0;
+	LOWL *list = malloc(sizeof(LOWL));
+ 	
+	if(list == 0){
+ 		return 1;
+	}
+	
+	OWN *element_1 = malloc(sizeof(OWN));
+	OWN *element_2 = malloc(sizeof(OWN));
+	OWN *element_3 = malloc(sizeof(OWN));
+ 
+ 	element_1->element = 52.24;
+ 	element_2->element = 1.4;
+ 	element_3->element = 4.2;
+ 	
+ 	element_1->next = element_2;
+ 	element_2->next = element_3;
+ 	element_3->next = 0;
+ 	
+ 	
+ 	list->first_element = element_1;
+ 	list->current_element = element_3; 
+ 	
+ 	printf("b4 saving\n");
+ 	
+ 	if(lowl_save(list, "list.check") != 0){//here problem
+ 		return 1;
+	}
+ 	
+ 	printf("had been saved\n");
+ 	
+ 	LOWL *list_load = lowl_load("list.check");
+    if(list_load == 0){
+        return 1;
+        printf("ERROR with loading");
+    }
+    
+    printf("had been loaded");
+
+    
+//    if(&list_load == 0){// also don't write
+//        return 1;
+//        printf("ERROR with loading");
+//    }
+ 	
+ 	OWN *tmp = list_load->first_element;
+    while (tmp != NULL) {
+    	count_2++;
+        printf("element %d: %.2f\n",count_2, tmp->element);
+        tmp = tmp->next;
+    }
+ 	
+}
+// last time it showed 佌䱗 䉐㌳㾳晦䂆
